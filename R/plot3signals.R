03bb464186d0515e79ba91f081c169bd64e40ee8
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-01-21 19:29:15 +0100

    Update plot3signals.R

--- R/plot3signals.R
+++ R/plot3signals.R
@@ -1,7 +1,9 @@
-plt.3signals <- function(signal1, signal2, signal3, name1='', name2='', name3='', col1='red', col2='green', col3='blue', xlim=NULL, main='maximale Fließhöhe', xlab='ID', ylab='Amplitude'){
-  plot(c(1, length(signal1)), c(min(signal1, signal2, signal3), max(signal1, signal2, signal3)), col="white", xlab = xlab, ylab = ylab, main = main, xlim=xlim)
+plt.3signals <- function(signal1, signal2, signal3, y=NULL, name1='', name2='', name3='', col1='red', col2='green', col3='blue', xlim=NULL, main='maximale Fließhöhe', xlab='ID', ylab='Amplitude'){
+  if (is.null(y))   plot(c(1, length(signal1)), c(min(signal1, signal2, signal3), max(signal1, signal2, signal3)), col="white", xlab = xlab, ylab = ylab, main = main, xlim=xlim)
+  if (!is.null(y))  plot(y, c(min(signal1, signal2, signal3), max(signal1, signal2, signal3)), col="white", xlab = xlab, ylab = ylab, main = main, xlim=xlim)
+  
   lines(signal1, type = "l", col = col1, )
   lines(signal2, type = "l", col = col2)
   lines(signal3, type = "l", col = col3)
-  legend("topright", c(name1, name2, name3), col = c(col1, col2, col3), lty = 1 , cex = 0.5)
+  legend("topright", c(name1, name2, name3), col = c(col1, col2, col3), lty = 1 , cex = cex)
 }
