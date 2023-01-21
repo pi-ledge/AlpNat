ad327fbf1857eb3c93f8d24f4fd80c60d10a35f9
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-01-21 19:15:05 +0100

    Update plot3signals.R

--- R/plot3signals.R
+++ R/plot3signals.R
@@ -1,7 +1,7 @@
-plt.3signals <- function(signal1, signal2, signal3, name1='', name2='', name3='', xlim=NULL){
-  plot(c(1, length(signal1)), c(min(signal1, signal2, signal3), max(signal1, signal2, signal3)), col="white", xlab = "timestep", ylab = "amplitude", main = "flow height\nmax", xlim=xlim)
-  lines(signal1, type = "l", col = "red", )
-  lines(signal2, type = "l", col = "blue")
-  lines(signal3, type = "l", col = "green")
-  legend("topright", c("unten", "oben_o", "oben_u"), col = c("red", "blue", 'green'), lty = 1 , cex = 0.5)
+plt.3signals <- function(signal1, signal2, signal3, name1='', name2='', name3='', col1='red', col2='green', col3='blue', xlim=NULL, main='maximale Fließhöhe', xlab='ID', ylab='Amplitude'){
+  plot(c(1, length(signal1)), c(min(signal1, signal2, signal3), max(signal1, signal2, signal3)), col="white", xlab = xlab, ylab = ylab, main = main, xlim=xlim)
+  lines(signal1, type = "l", col = col1, )
+  lines(signal2, type = "l", col = col2)
+  lines(signal3, type = "l", col = col3)
+  legend("topright", c(name1, name2, name3), col = c(col1, col2, col3), lty = 1 , cex = 0.5)
 }
